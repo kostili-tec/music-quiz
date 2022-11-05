56739e16b38c8048631a2bcd31acf80d0294338e
kostili-tec <weh4teyou@gmail.com>
2022-11-05 20:16:58 +0300

    feat: add section with bird img and audio player

--- songbird/src/js/pages/game/gamePage.js
+++ songbird/src/js/pages/game/gamePage.js
@@ -17,6 +17,70 @@ const createListBirds = () => {
   return ul;
 }
 
+const createPlayer = () => {
+  const playerContainer = document.createElement('div');
+  playerContainer.classList.add('player-container');
+
+  const birdImg = document.createElement('img');
+  birdImg.classList.add('birds-img');
+  birdImg.src = './copies/bird.jpg';
+
+  const rightContainer = document.createElement('div');
+  rightContainer.classList.add('right-container');
+
+  const nameBird = document.createElement('h3');
+  nameBird.textContent = '*******';
+  nameBird.classList.add('name-bird__h3');
+
+  const audioPlayer = document.createElement('div');
+  audioPlayer.classList.add('audio-container');
+
+  const playButton = document.createElement('button');
+  playButton.classList.add('play-button');
+
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.classList.add('icon-svg');
+  svg.innerHTML = `<use xlink:href="./copies/sprite.svg#pause-button"></use>`;
+
+  playButton.append(svg);
+
+  const progressContainer = document.createElement('div');
+  progressContainer.classList.add('progress-container');
+
+  const progresBar = document.createElement('div');
+  progresBar.classList.add('progres-bar');
+  const progress = document.createElement('div');
+  progress.classList.add('progress');
+  progresBar.append(progress);
+
+  const timeCointaer = document.createElement('div');
+  timeCointaer.classList.add('time-container');
+
+  const spanContainer = document.createElement('span-container');
+  spanContainer.classList.add('span-container');
+
+  const spanStart = document.createElement('span');
+  const spanEnd = document.createElement('span');
+
+  spanStart.classList.add('audio-current');
+  spanStart.textContent = '00:00';
+  spanEnd.classList.add('audio-end');
+  spanEnd.textContent = '00:00';
+
+  spanContainer.append(spanStart, spanEnd);
+
+  timeCointaer.append(spanStart, spanEnd);
+
+  progressContainer.append(progresBar, timeCointaer);
+
+  audioPlayer.append(playButton, progressContainer);
+  rightContainer.append(nameBird, audioPlayer);
+
+  playerContainer.append(birdImg, rightContainer);
+
+  return playerContainer;
+}
+
 export const createGame = () => {
   const main = document.createElement('main');
   main.classList.add('main');
@@ -24,6 +88,8 @@ export const createGame = () => {
   navContainer.classList.add('birds-container');
   navContainer.append(createListBirds());
 
-  main.append(navContainer);
+  const audioPlayer = createPlayer();
+
+  main.append(navContainer, audioPlayer);
   document.body.append(main);
 }
